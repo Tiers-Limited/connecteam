2eb28706b8541cbf479d6b66556c6458a440b3e7
Sohaib Ikram <2022CS621@student.uet.edu.pk>
2026-03-18 15:47:45 +0500

    deactivate the location

--- backend/scripts/seedLocations.js
+++ backend/scripts/seedLocations.js
@@ -6,6 +6,9 @@ const { LOCATIONS } = require('../utils/constants');
  * Used for Connecteams sync; do not add locations manually for staff tips.
  */
 async function seedLocations() {
+  const configuredNames = LOCATIONS.map((l) => l.name);
+
+  // Ensure configured locations are active
   for (const { name } of LOCATIONS) {
     await Location.findOneAndUpdate(
       { name },
@@ -13,6 +16,12 @@ async function seedLocations() {
       { upsert: true, new: true }
     );
   }
+
+  // Deactivate any locations that are no longer in the configured list
+  await Location.updateMany(
+    { name: { $nin: configuredNames } },
+    { isActive: false }
+  );
 }
 
 module.exports = { seedLocations };
